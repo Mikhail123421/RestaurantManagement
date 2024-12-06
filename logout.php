c48258f4d79e5fd2d7aa7e25cf3f00b406faef26
Mikhail123421 <71553599+Mikhail123421@users.noreply.github.com>
2024-12-06 21:53:35 +0330

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,11 +1,15 @@
 <?php
 session_start(); 
 
-
+// حذف همه مقادیر سشن
 session_unset();
 session_destroy();
 
+// ابطال کوکی‌ها
+setcookie('user_email', '', time() - 3600, "/");
+setcookie('user_id', '', time() - 3600, "/");
 
+// انتقال به صفحه ورود
 header('Location: login.php');
 exit;
 ?>
